72f996f203cac001360f56abac53b51f17bb805a
pilaftank <pilaftank@380ab6fc-8a2d-0410-b153-0d49f6b511e5>
2008-09-11 05:47:45 +0000

    Added bat file to kick off ant on Windows.

---
+++ src/tools/build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem ########################################################
+rem ##  Snap Backup                                       ##
+rem ##  Build on Microsoft Windows                        ##
+rem ########################################################
+
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_07
+set ANT_HOME=C:\Apps\Ant\apache-ant-1.7.1
+set JAVA_HOME
+set ANT_HOME
+call %ANT_HOME%\bin\ant build
+pause
