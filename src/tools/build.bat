b9ead25a4d84425014e1077aaf291dadc3dfc03e
pilaftank <pilaftank@380ab6fc-8a2d-0410-b153-0d49f6b511e5>
2009-05-15 07:03:57 +0000

    Bumped up JDK to 1.6.0_13.

--- src/tools/build.bat
+++ src/tools/build.bat
@@ -5,7 +5,7 @@ rem ##  Snap Backup                                       ##
 rem ##  Build on Microsoft Windows                        ##
 rem ########################################################
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_07
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_13
 set ANT_HOME=C:\Apps\Ant\apache-ant-1.7.1
 set JAVA_HOME
 set ANT_HOME
