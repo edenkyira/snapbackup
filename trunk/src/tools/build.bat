e82336f807fb63de934f6fa5afeabd7792d2fbfe
pilaftank <pilaftank@380ab6fc-8a2d-0410-b153-0d49f6b511e5>
2009-05-15 23:11:28 +0000

    Added code to auto detect JDK.

--- trunk/src/tools/build.bat
+++ trunk/src/tools/build.bat
@@ -1,11 +1,16 @@
 @echo off
+::::::::::::::::::::::::::::::::
+:: Snap Backup                ::
+:: Build on Microsoft Windows ::
+::::::::::::::::::::::::::::::::
 
-rem ########################################################
-rem ##  Snap Backup                                       ##
-rem ##  Build on Microsoft Windows                        ##
-rem ########################################################
+:: Set JAVA_HOME to current JDK (note: delim ends in a tab)
+set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit
+set Cmd=reg query "%KeyName%" /v CurrentVersion
+for /f "tokens=2 delims=REG_SZ	" %%T in ('%Cmd%') do set VersionJDK=%%T
+set Cmd=reg query "%KeyName%\%VersionJDK%" /v JavaHome
+for /f "tokens=2,3 delims=REG_SZ	" %%i in ('%Cmd%') do set JAVA_HOME=%%i_%%j
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_13
 set ANT_HOME=C:\Apps\Ant\apache-ant-1.7.1
 set JAVA_HOME
 set ANT_HOME
