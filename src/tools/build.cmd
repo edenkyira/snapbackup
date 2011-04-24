26b45522f9e24726fff7336cf68bddb186abf15f
pilaftank <pilaftank@gmail.com>
2011-04-24 06:41:35 +0000

    Added copy exe to VirtualBox folder.

--- src/tools/build.cmd
+++ src/tools/build.cmd
@@ -10,8 +10,16 @@ for /d %%i in ("\Program Files\Java\jdk*") do set JAVA_HOME=%%i
 :: set ANT_HOME
 for /d %%i in ("\Apps\Ant\apache-ant*") do set ANT_HOME=%%i
 
-:: Display Variables and Launch Ant
+:: Display Variables and launch Ant
 set JAVA_HOME
 set ANT_HOME
 call %ANT_HOME%\bin\ant build
+
+:: Copy to VirvualBox folder
+if exist \\VBOXSVR\VirtualBox_Share (
+   echo.
+   copy ..\..\build\SnapBackupInstaller.exe \\VBOXSVR\VirtualBox_Share
+   dir \\VBOXSVR\VirtualBox_Share\SnapBackupInstaller.exe
+   )
+echo.
 pause
